a35176db0b48d94ad27e0ff3ebed4e86ef0123d2
chouaibAtm <atmanichouaib22@gmail.com>
2025-03-14 19:58:19 +0100

    Remove unused SVG files and add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    "text-custom-blue",
+    "text-custom-violet",
+    "text-custom-pink",
+    "text-custom-orange",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
